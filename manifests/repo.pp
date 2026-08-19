c3dd667305677be6dee05d6e0846cab507ab737b
Arjen Heidinga <dexter@beetjevreemd.nl>
2026-08-19 17:03:21 +0200

    fix:  Error
    Error while evaluating a Resource Statement, Apt::Source[zabbix]: parameter 'architecture' expects a value of type Undef or String, got Tuple (file: /etc/puppetlabs/code/environments/production/modules/zabbix/manifests/repo.pp, line: 122)

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -124,7 +124,7 @@ class zabbix::repo (
           repos    => 'main',
           release  => $releasename,
           pin      => $pinpriority,
-          architecture   => ['amd64'],
+          architecture   => 'amd64',
           require  => [
             Apt_key['zabbix-FBABD5F'],
             Apt_key['zabbix-A1848F5'],
